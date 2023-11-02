534a9266e564a4cac66a2fca97064214e10f223a
Bernardo Garza <bernimgarza@gmail.com>
2023-11-01 22:44:36 -0500

    Added the first files for mapping.

---
+++ mappings.lua
@@ -0,0 +1,9 @@
+return {
+  -- Keymappings for insert mode.
+  i = {
+    -- Setting the Ctrl+Backspace to delete the previous word while in insert mode.
+    ["<C-H>"] = { "<C-W>" },
+  },
+  n = {
+  },
+}
